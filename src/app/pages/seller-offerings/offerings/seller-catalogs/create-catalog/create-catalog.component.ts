6efbd5476e71284a0f28bdf7175d0f2725b4c259
Miguel Ortega <miguel.ortega@seamware.com>
2026-06-08 17:11:36 +0200

    update catalogs to use stepper

--- src/app/pages/seller-offerings/offerings/seller-catalogs/create-catalog/create-catalog.component.ts
+++ src/app/pages/seller-offerings/offerings/seller-catalogs/create-catalog/create-catalog.component.ts
@@ -1,18 +1,19 @@
-import { Component, OnInit, ChangeDetectorRef, HostListener, ElementRef, ViewChild, OnDestroy } from '@angular/core';
-import { Router } from '@angular/router';
-import { ApiServiceService } from 'src/app/services/product-service.service';
-import {LocalStorageService} from "src/app/services/local-storage.service";
-import {EventMessageService} from "src/app/services/event-message.service";
-import { LoginInfo } from 'src/app/models/interfaces';
-import { FormGroup, FormControl, Validators } from '@angular/forms';
-import moment from 'moment';
-import { noWhitespaceValidator } from 'src/app/validators/validators';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import * as moment from 'moment';
 import { Subject } from 'rxjs';
 import { takeUntil } from 'rxjs/operators';
-
-import {components} from "src/app/models/product-catalog";
+import { LoginInfo } from 'src/app/models/interfaces';
+import { EventMessageService } from 'src/app/services/event-message.service';
+import { LocalStorageService } from 'src/app/services/local-storage.service';
+import { ApiServiceService } from 'src/app/services/product-service.service';
+import { noWhitespaceValidator } from 'src/app/validators/validators';
 import { environment } from 'src/environments/environment';
-type Catalog_Create = components["schemas"]["Catalog_Create"];
+
+import { components } from 'src/app/models/product-catalog';
+import { StepChangedEvent } from '../../../../../shared/stepper/stepper.component';
+
+type Catalog_Create = components['schemas']['Catalog_Create'];
 
 @Component({
   selector: 'create-catalog',
@@ -20,84 +21,65 @@ type Catalog_Create = components["schemas"]["Catalog_Create"];
   styleUrl: './create-catalog.component.css'
 })
 export class CreateCatalogComponent implements OnInit, OnDestroy {
-  partyId:any='';
 
-  catalogToCreate:Catalog_Create | undefined;
-
-  stepsElements:string[]=['general-info','summary'];
-  stepsCircles:string[]=['general-circle','summary-circle'];
+  partyId: any = '';
+  catalogToCreate: Catalog_Create | undefined;
   currentStep = 0;
-  highestStep = 0;
-  steps = [
-    'General Info',
-    'Summary'
-  ];
+  loading = false;
 
-  //markdown variables:
-  showPreview:boolean=false;
-  showEmoji:boolean=false;
-  description:string='';  
+  steps = ['General Info', 'Summary'];
 
-  //CONTROL VARIABLES:
-  showGeneral:boolean=true;
-  showSummary:boolean=false;
-  //Check if step was done
-  generalDone:boolean=false;
-  finishDone:boolean=false;
-  loading:boolean=false;
-
-  //SERVICE GENERAL INFO:
   generalForm = new FormGroup({
     name: new FormControl('', [Validators.required, Validators.maxLength(100), noWhitespaceValidator]),
     description: new FormControl('', Validators.maxLength(100000)),
   });
 
-  errorMessage:any='';
-  showError:boolean=false;
+  errorMessage: any = '';
+  showError = false;
   private destroy$ = new Subject<void>();
 
   constructor(
-    private router: Router,
-    private cdr: ChangeDetectorRef,
     private localStorage: LocalStorageService,
     private eventMessage: EventMessageService,
-    private elementRef: ElementRef,
-    private api: ApiServiceService
+    private api: ApiServiceService,
   ) {
     this.eventMessage.messages$
-    .pipe(takeUntil(this.destroy$))
-    .subscribe(ev => {
-      if(ev.type === 'ChangedSession') {
-        this.initPartyInfo();
-      }
-    })
-  }
-
-  @HostListener('document:click')
-  onClick() {
-    if(this.showEmoji==true){
-      this.showEmoji=false;
-      this.cdr.detectChanges();
-    }
+      .pipe(takeUntil(this.destroy$))
+      .subscribe(ev => {
+        if (ev.type === 'ChangedSession') {
+          this.initPartyInfo();
+        }
+      });
   }
 
   ngOnInit() {
     this.initPartyInfo();
   }
 
-  ngOnDestroy(){
+  ngOnDestroy() {
     this.destroy$.next();
     this.destroy$.complete();
   }
 
-  initPartyInfo(){
-    let aux = this.localStorage.getObject('login_items') as LoginInfo;
-    if(JSON.stringify(aux) != '{}' && (((aux.expire - moment().unix())-4) > 0)) {
-      if(aux.logged_as==aux.id){
+  get canAdvance(): boolean {
+    return this.generalForm?.valid ?? false;
+  }
+
+  onStepChanged(event: StepChangedEvent): void {
+    this.currentStep = event.step;
+    if (event.isLastStep) {
+      this.setCatalogData();
+    }
+  }
+
+  initPartyInfo() {
+    const aux = this.localStorage.getObject('login_items') as LoginInfo;
+    if (JSON.stringify(aux) !== '{}' && ((aux.expire - moment().unix()) - 4) > 0) {
+      if (aux.logged_as === aux.id) {
         this.partyId = aux.partyId;
       } else {
-        let loggedOrg = aux.organizations.find((element: { id: any; }) => element.id == aux.logged_as)
-        this.partyId = loggedOrg.partyId
+        const loggedOrg = aux.organizations.find((element: { id: any }) => element.id === aux.logged_as);
+        this.partyId = loggedOrg.partyId;
       }
     }
   }
@@ -106,250 +88,41 @@ export class CreateCatalogComponent implements OnInit, OnDestroy {
     this.eventMessage.emitSellerCatalog(true);
   }
 
-  toggleGeneral(){
-    this.selectStep('general-info','general-circle');
-    this.showGeneral=true;
-    this.showSummary=false;
-    this.showPreview=false;
-  }
-
-  showFinish(){
-    this.finishDone=true;
-    this.setCatalogData();
-    this.showGeneral=false;
-    this.showSummary=true;
-    this.selectStep('summary','summary-circle');
-    this.showPreview=false;
-  }
-
-  setCatalogData(){
-    if(this.generalForm.value.name!=null){
-      this.catalogToCreate={
+  setCatalogData() {
+    if (this.generalForm.value.name != null) {
+      this.catalogToCreate = {
         name: this.generalForm.value.name,
-        description: this.generalForm.value.description != null ? this.generalForm.value.description : '',
-        lifecycleStatus: "Active",
-        relatedParty: [
-          {
-              id: this.partyId,
-              //href: "http://proxy.docker:8004/party/individual/urn:ngsi-ld:individual:803ee97b-1671-4526-ba3f-74681b22ccf3",
-              role: environment.SELLER_ROLE,
-              "@referredType": ''
-          }
-        ],
-      }
-      console.log('CATALOG TO CREATE:')
-      console.log(this.catalogToCreate)
+        description: this.generalForm.value.description ?? '',
+        lifecycleStatus: 'Active',
+        relatedParty: [{
+          id: this.partyId,
+          role: environment.SELLER_ROLE,
+          '@referredType': '',
+        }],
+      };
     }
   }
 
-  createCatalog(){
-    this.setCatalogData();
-    this.loading=true;
+  createCatalog() {
+    this.loading = true;
     this.api.postCatalog(this.catalogToCreate).subscribe({
-      next: data => {
-        this.loading=false;
+      next: () => {
+        this.loading = false;
         this.goBack();
       },
       error: error => {
-        console.error('There was an error while updating!', error);
-        if(error.error.error){
-          console.log(error)
-          this.errorMessage='Error: '+error.error.error;
-        } else {
-          this.errorMessage='There was an error while creating the catalog!';
-        }
-        this.loading=false;
-        this.showError=true;
-        setTimeout(() => {
-          this.showError = false;
-        }, 3000);
-      }
-    })
-  }
-
-  //STEPS METHODS
-  removeClass(elem: HTMLElement, cls:string) {
-    var str = " " + elem.className + " ";
-    elem.className = str.replace(" " + cls + " ", " ").replace(/^\s+|\s+$/g, "");
-  }
-
-  addClass(elem: HTMLElement, cls:string) {
-      elem.className += (" " + cls);
-  }
-
-  unselectMenu(elem:HTMLElement | null,cls:string){
-    if(elem != null){
-      if(elem.className.match(cls)){
-        this.removeClass(elem,cls)
-      } else {
-        console.log('already unselected')
-      }
-    }
-  }
-
-  selectMenu(elem:HTMLElement| null,cls:string){
-    if(elem != null){
-      if(elem.className.match(cls)){
-        console.log('already selected')
-      } else {
-        this.addClass(elem,cls)
-      }
-    }
-  }
-  
-  //STEPS CSS EFFECTS:
-  selectStep(step:string,stepCircle:string){
-    const index = this.stepsElements.findIndex(item => item === step);
-    if (index !== -1) {
-      this.stepsElements.splice(index, 1);
-      this.selectMenu(document.getElementById(step),'text-primary-100 dark:text-primary-50')
-      this.unselectMenu(document.getElementById(step),'text-gray-500') 
-      for(let i=0; i<this.stepsElements.length;i++){
-        this.unselectMenu(document.getElementById(this.stepsElements[i]),'text-primary-100 dark:text-primary-50')
-        this.selectMenu(document.getElementById(this.stepsElements[i]),'text-gray-500') 
-      }
-      this.stepsElements.push(step);
-    }
-    const circleIndex = this.stepsCircles.findIndex(item => item === stepCircle);
-    if (index !== -1) {
-      this.stepsCircles.splice(circleIndex, 1);
-      this.selectMenu(document.getElementById(stepCircle),'border-primary-100 dark:border-primary-50')
-      this.unselectMenu(document.getElementById(stepCircle),'border-gray-400');
-      for(let i=0; i<this.stepsCircles.length;i++){
-        this.unselectMenu(document.getElementById(this.stepsCircles[i]),'border-primary-100 dark:border-primary-50')
-        this.selectMenu(document.getElementById(this.stepsCircles[i]),'border-gray-400');
-      }
-      this.stepsCircles.push(stepCircle);
-    }
-  }
-
-  //Markdown actions:
-  addBold() {
-    const currentText = this.generalForm.value.description;
-    this.generalForm.patchValue({
-      description: currentText + ' **bold text** '
+        console.error('There was an error while creating the catalog!', error);
+        this.errorMessage = error.error?.error
+          ? 'Error: ' + error.error.error
+          : 'There was an error while creating the catalog!';
+        this.loading = false;
+        this.showError = true;
+        setTimeout(() => { this.showError = false; }, 3000);
+      },
     });
-  }
-
-  addItalic() {
-    const currentText = this.generalForm.value.description;
-    this.generalForm.patchValue({
-      description: currentText + ' _italicized text_ '
-    });
-  }
-
-  addList(){
-    const currentText = this.generalForm.value.description;
-    this.generalForm.patchValue({
-      description: currentText + '\n- First item\n- Second item'
-    });    
-  }
-
-  addOrderedList(){
-    const currentText = this.generalForm.value.description;
-    this.generalForm.patchValue({
-      description: currentText + '\n1. First item\n2. Second item'
-    });    
-  }
-
-  addCode(){
-    const currentText = this.generalForm.value.description;
-    this.generalForm.patchValue({
-      description: currentText + '\n`code`'
-    });    
-  }
-
-  addCodeBlock(){
-    const currentText = this.generalForm.value.description;
-    this.generalForm.patchValue({
-      description: currentText + '\n```\ncode\n```'
-    }); 
-  }
-
-  addBlockquote(){
-    const currentText = this.generalForm.value.description;
-    this.generalForm.patchValue({
-      description: currentText + '\n> blockquote'
-    });    
-  }
-
-  addLink(){
-    const currentText = this.generalForm.value.description;
-    this.generalForm.patchValue({
-      description: currentText + ' [title](https://www.example.com) '
-    });    
-  } 
-
-  addTable(){
-    const currentText = this.generalForm.value.description;
-    this.generalForm.patchValue({
-      description: currentText + '\n| Syntax | Description |\n| ----------- | ----------- |\n| Header | Title |\n| Paragraph | Text |'
-    });
-  }
-
-  addEmoji(event:any){
-    console.log(event)
-    this.showEmoji=false;
-    const currentText = this.generalForm.value.description;
-    this.generalForm.patchValue({
-      description: currentText + event.emoji.native
-    });
-  }
-
-  togglePreview(){
-    if(this.generalForm.value.description){
-      this.description=this.generalForm.value.description;
-    } else {
-      this.description=''
-    }
   }
 
   hasLongWord(str: string | undefined, threshold = 20) {
-    if(str){
-      return str.split(/\s+/).some(word => word.length > threshold);
-    } else {
-      return false
-    }   
-  }
-
-  goToStep(index: number) {
-    // Solo validar en modo creación
-    if (index > this.currentStep) {
-      // Validar el paso actual
-      const currentStepValid = this.validateCurrentStep();
-      if (!currentStepValid) {
-        return; // No permitir avanzar si el paso actual no es válido
-      }
-    }
-    
-    this.currentStep = index;
-    if(this.currentStep>this.highestStep){
-      this.highestStep=this.currentStep
-    }
-
-    if(this.currentStep==1){
-      this.showFinish();
-    }
-  }
-
-  validateCurrentStep(): boolean {
-    switch (this.currentStep) {
-      case 0: // General Info
-        return this.generalForm?.valid || false;
-      case 1: // Product Specification
-        return true;
-      default:
-        return true;
-    }
-  }
-
-  canNavigate(index: number) {
-    return (this.generalForm?.valid &&  (index <= this.currentStep)) || (this.generalForm?.valid &&  (index <= this.highestStep));
-  }  
-
-  handleStepClick(index: number): void {
-    if (this.canNavigate(index)) {
-      this.goToStep(index);
-    }
+    return str ? str.split(/\s+/).some(word => word.length > threshold) : false;
   }
 }
