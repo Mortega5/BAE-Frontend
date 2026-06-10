4a56bcaa9e8bc8e1741865d5d83c34956de502c4
Miguel Ortega <miguel.ortega@seamware.com>
2026-06-10 17:15:14 +0200

    create a specification-chrac-form to share between components

---
+++ src/app/shared/forms/characteristic-value-spec/characteristic-value-spec-form.component.ts
@@ -0,0 +1,42 @@
+import { Component, Input } from '@angular/core';
+import { FormGroup } from '@angular/forms';
+import { FormField } from 'src/app/models/formFields/form-field.model';
+import { DynamicFormComponent } from '../dynamic-form/dynamic-form.component';
+
+export type CharValueType = 'string' | 'number' | 'range';
+
+@Component({
+  selector: 'app-characteristic-value-spec-form',
+  templateUrl: './characteristic-value-spec-form.component.html',
+  standalone: true,
+  imports: [DynamicFormComponent],
+})
+export class CharacteristicValueSpecFormComponent {
+  @Input() valueType!: CharValueType;
+  @Input() form!: FormGroup;
+  @Input() readonly: boolean = false;
+
+  get fields(): FormField[] {
+    const ro = this.readonly;
+    const common: FormField[] = [
+      { type: 'boolean', name: 'isDefault', label: 'CHAR_SPEC._is_default', readonly: ro }
+    ];
+    switch (this.valueType) {
+      case 'string':
+        return [...common,
+        { type: 'string', name: 'value', label: 'CHAR_SPEC._value', required: true, readonly: ro }
+        ];
+      case 'number':
+        return [...common,
+        { type: 'number', name: 'value', label: 'CHAR_SPEC._value', required: true, readonly: ro, colSpan: 2 },
+        { type: 'string', name: 'unitOfMeasure', label: 'CHAR_SPEC._unit', readonly: ro, colSpan: 1 }
+        ];
+      case 'range':
+        return [...common,
+        { type: 'number', name: 'valueFrom', label: 'CHAR_SPEC._value_from', required: true, readonly: ro, colSpan: 1 },
+        { type: 'number', name: 'valueTo', label: 'CHAR_SPEC._value_to', required: true, readonly: ro, colSpan: 1 },
+        { type: 'string', name: 'unitOfMeasure', label: 'CHAR_SPEC._unit', required: true, readonly: ro, colSpan: 1 }
+        ];
+    }
+  }
+}
