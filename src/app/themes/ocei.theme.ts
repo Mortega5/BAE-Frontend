e84003c777912c894b7997f5722a66be5a440b05
Miguel Ortega <miguel.ortega@seamware.com>
2026-07-29 11:31:32 +0200

    add ocei theme

---
+++ src/app/themes/ocei.theme.ts
@@ -0,0 +1,46 @@
+import { NavHeaderLink, NavLink, ThemeConfig } from './theme.interfaces';
+
+// Define navigation links for the theme
+const oceiHeaderLinks: NavLink[] = [
+  {
+    label: 'HEADER._home', // Translation key
+    url: '/dashboard',
+    isRouterLink: true
+  },
+  {
+    label: 'HEADER._browse',
+    id: 'searchDropdown', // ID para el toggle de Flowbite
+    children: [
+      { label: 'HEADER._services', url: '/search', isRouterLink: true },
+      { label: 'HEADER._catalogs', url: '/catalogues', isRouterLink: true },
+      { label: 'HEADER._blueprints', url: 'https://marketplace.pre.o-cei.eu/search/urn:ngsi-ld:product-offering:bd1bb611-0b9e-4e79-ba25-abf1240039e1', isRouterLink: false }
+    ]
+  }
+];
+
+const oceiFooterLinks: NavHeaderLink[] = [
+
+];
+
+// Export the main theme configuration object
+export const OCEI_THEME_CONFIG: ThemeConfig = {
+  name: 'Ocei', // This MUST match the name used in the SCSS class (theme-galaxy)
+  displayName: 'OCEI Marketplace',
+  isDefault: true,
+  assets: {
+    logoUrl: 'assets/themes/ocei/ocei-logo.png',
+    jumboBgUrl: 'assets/themes/ocei/jumboBackground.png',
+    cardDefaultBgUrl: 'assets/themes/ocei/cardBackground.png'
+  },
+  links: {
+    headerLinks: oceiHeaderLinks,
+    footerLinks: oceiFooterLinks,
+
+    linkedin: 'https://www.linkedin.com/company/o-cei-horizon',
+    youtube: 'https://www.youtube.com/@o-cei'
+  },
+  dashboard: {
+    showFeaturedOfferings: true,
+    showPlatformBenefits: false,
+  }
+};
