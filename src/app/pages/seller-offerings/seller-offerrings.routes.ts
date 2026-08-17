21ffecbae49323b111ff0f16a1ddecbcaa09d18b
Miguel Ortega <miguel.ortega@seamware.com>
2026-08-17 13:59:55 +0200

    disable software route

--- src/app/pages/seller-offerings/seller-offerrings.routes.ts
+++ src/app/pages/seller-offerings/seller-offerrings.routes.ts
@@ -11,11 +11,8 @@ import { SellerProductSpecComponent } from './offerings/seller-product-spec/sell
 import { UpdateProductSpecComponent } from './offerings/seller-product-spec/update-product-spec/update-product-spec.component';
 import { ResourceSpecFormComponent } from './offerings/seller-resource-spec/resource-spec-form/resource-spec-form.component';
 import { SellerResourceSpecComponent } from './offerings/seller-resource-spec/seller-resource-spec.component';
-import { ServiceSpecFormComponent } from './offerings/seller-service-spec/service-spec-form/service-spec-form.component';
 import { SellerServiceSpecComponent } from './offerings/seller-service-spec/seller-service-spec.component';
-import { CreateSoftwareComponent } from './offerings/seller-software/create-software/create-software.component';
-import { SellerSoftware } from './offerings/seller-software/seller-software';
-import { UpdateSoftwareComponent } from './offerings/seller-software/update-software/update-software.component';
+import { ServiceSpecFormComponent } from './offerings/seller-service-spec/service-spec-form/service-spec-form.component';
 import { SellerOfferingsPaths } from './seller-offerings.paths';
 
 const { segments } = SellerOfferingsPaths;
@@ -39,14 +36,14 @@ export const sellerOfferingsRoutes: Routes = [
       { path: segments.id, component: UpdateOfferComponent },
     ],
   },
-  {
-    path: segments.softwares,
-    children: [
-      { path: '', component: SellerSoftware },
-      { path: segments.new, component: CreateSoftwareComponent },
-      { path: segments.id, component: UpdateSoftwareComponent },
-    ],
-  },
+  // {
+  //   path: segments.softwares,
+  //   children: [
+  //     { path: '', component: SellerSoftware },
+  //     { path: segments.new, component: CreateSoftwareComponent },
+  //     { path: segments.id, component: UpdateSoftwareComponent },
+  //   ],
+  // },
   {
     path: segments.productSpecs,
     children: [
