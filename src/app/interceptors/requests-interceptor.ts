eaf30743a501a1d6c0924f3336e9a8883b0a334a
Miguel Ortega <miguel.ortega@seamware.com>
2026-09-09 13:46:38 +0200

    fix: reset session on 401 responses in the request interceptor

--- src/app/interceptors/requests-interceptor.ts
+++ src/app/interceptors/requests-interceptor.ts
@@ -3,10 +3,12 @@ import {
   HttpRequest,
   HttpHandler,
   HttpEvent,
+  HttpErrorResponse,
   HttpInterceptor,
 } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Observable, catchError, throwError } from 'rxjs';
 import {LocalStorageService} from "../services/local-storage.service";
+import { EventMessageService } from "../services/event-message.service";
 import { LoginInfo } from '../models/interfaces';
 import moment from 'moment';
 import { environment } from 'src/environments/environment';
@@ -26,52 +28,47 @@ export class RequestInterceptor implements HttpInterceptor {
     public static API_ORDERING: String = environment.PRODUCT_ORDER;
     public static ORDER_LIMIT: Number = environment.ORDER_LIMIT;
 
-    constructor(private localStorage: LocalStorageService) { }
-  
+    constructor(
+        private localStorage: LocalStorageService,
+        private eventMessage: EventMessageService
+    ) { }
+
     intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
         let aux = this.localStorage.getObject('login_items') as LoginInfo;
-        if(JSON.stringify(aux) != '{}' && (((aux.expire - moment().unix())-4) > 0)) {
+        const isLoggedIn = JSON.stringify(aux) != '{}' && (((aux.expire - moment().unix())-4) > 0);
+
+        if(isLoggedIn) {
             if (!this.shouldAttachAuthHeaders(request.url)) {
                 return next.handle(request);
             }
 
-            if(aux.logged_as != aux.id){
-                let modifiedRequest = request.clone({
-                    setHeaders: {
-                    'Authorization': 'Bearer '+aux.token,
-                    'X-Organization': aux.logged_as
-                    },
-                });
-                if (request.url.startsWith(`${RequestInterceptor.BASE_URL}${RequestInterceptor.API_ORDERING}/productOrder`)) {
-                    modifiedRequest = request.clone({
-                        setHeaders: {
-                        'Authorization': 'Bearer '+aux.token,
-                        'X-Organization': aux.logged_as,
-                        'X-Terms-Accepted': 'true'
-                        },
-                    });
-                }  
-                return next.handle(modifiedRequest);
-            } else {
-                let modifiedRequest = request.clone({
-                    setHeaders: {
-                    'Authorization': 'Bearer '+aux.token,
-                    },
-                });
-                if (request.url.startsWith(`${RequestInterceptor.BASE_URL}${RequestInterceptor.API_ORDERING}/productOrder`)) {
-                    modifiedRequest = request.clone({
-                        setHeaders: {
-                        'Authorization': 'Bearer '+aux.token,
-                        'X-Terms-Accepted': 'true'
-                        },
-                    });
-                }  
-                return next.handle(modifiedRequest);
-            }            
+            const isOrgSession = aux.logged_as != aux.id;
+            const isOrderRequest = request.url.startsWith(`${RequestInterceptor.BASE_URL}${RequestInterceptor.API_ORDERING}/productOrder`);
+
+            const headers: Record<string, string> = { 'Authorization': 'Bearer ' + aux.token };
+            if (isOrgSession) headers['X-Organization'] = aux.logged_as;
+            if (isOrderRequest) headers['X-Terms-Accepted'] = 'true';
+
+            const modifiedRequest = request.clone({ setHeaders: headers });
+            return next.handle(modifiedRequest).pipe(
+                catchError(error => this.handleAuthError(error))
+            );
         } else {
             console.log('not logged')
             return next.handle(request);
-        } 
+        }
+    }
+
+    // A 401 on an authenticated request means the session is no longer valid
+    // server-side (revoked/expired token); reset it locally and let whoever
+    // reacts to 'LoginProcess' (header, refresh loop) pick up the change,
+    // instead of silently keep failing while the app still thinks it's logged in.
+    private handleAuthError(error: unknown) {
+        if (error instanceof HttpErrorResponse && error.status === 401) {
+            this.localStorage.setObject('login_items', {});
+            this.eventMessage.emitLogin({} as LoginInfo);
+        }
+        return throwError(() => error);
     }
 
     private shouldAttachAuthHeaders(requestUrl: string): boolean {
