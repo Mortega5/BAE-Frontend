2169db5f6d10646a73fac000fdc035026e32a151
Miguel Ortega <miguel.ortega@seamware.com>
2026-09-09 16:38:31 +0200

    fix: refresh Profile > General form when switching session role

--- src/app/pages/user-profile/profile-sections/profile-general/profile-general.component.ts
+++ src/app/pages/user-profile/profile-sections/profile-general/profile-general.component.ts
@@ -1,16 +1,41 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subject } from 'rxjs';
+import { takeUntil } from 'rxjs/operators';
+import { EventMessageService } from 'src/app/services/event-message.service';
 import { LocalStorageService } from 'src/app/services/local-storage.service';
 
 @Component({
   selector: 'app-profile-general',
   templateUrl: './profile-general.component.html',
 })
-export class ProfileGeneralComponent implements OnInit {
+export class ProfileGeneralComponent implements OnInit, OnDestroy {
   loggedAsUser: boolean = true;
 
-  constructor(private localStorage: LocalStorageService) { }
+  private destroy$ = new Subject<void>();
+
+  constructor(
+    private localStorage: LocalStorageService,
+    private eventMessage: EventMessageService
+  ) { }
 
   ngOnInit() {
+    this.refreshLoggedAsUser();
+
+    this.eventMessage.messages$
+      .pipe(takeUntil(this.destroy$))
+      .subscribe(ev => {
+        if (ev.type === 'ChangedSession') {
+          this.refreshLoggedAsUser();
+        }
+      });
+  }
+
+  ngOnDestroy() {
+    this.destroy$.next();
+    this.destroy$.complete();
+  }
+
+  private refreshLoggedAsUser() {
     const aux = this.localStorage.getValidLoginInfo();
     if (aux) {
       this.loggedAsUser = aux.logged_as == aux.id;
